de39f6d434be3e72954e3df155c0f25dbf93e763
cynjean <cynthia.2056@yahoo.com>
2026-03-19 04:17:23 -0500

    Objects Lab Completed

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-//Write your code here
+// Managing Event Attendees
 
+// Declare Attendee Object
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00 
+}
 
+// Log Attendee Name
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
 
+// Log Ticket Price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+} 
+
+// Update Ticket Type
+function updateTicketType(attendee, newTicket) {
+  attendee.ticketType = newTicket;
+}
+
+// Update Ticket Price
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+// Remove Event Property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+// Add Checked-In Property
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+} 
 //Needed for the tests to work. Don't modify
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
